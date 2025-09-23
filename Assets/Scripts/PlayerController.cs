9434e4013acf3f1ee21e86303a67dadcf12fcd88
WesleyBurgt <wesleyburgt@gmail.com>
2025-09-23 15:50:12 +0200

    [FIX] landing collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,10 @@ public class PlayerController : MonoBehaviour
     public float JumpForce = 1.0f;
     public float JumpCooldown = 0.5f;
 
+    [Header("Collision")]
+    public Camera Camera;
+    public float distance;
+
     Rigidbody m_Rigidbody;
     Collider m_Collider;
 
@@ -24,6 +28,8 @@ public class PlayerController : MonoBehaviour
     float rotateCountdown = -1f;
     float jumpCountdown = -1f;
     bool isGrounded = false;
+
+    RaycastHit hit;
 
     void Start()
     {
@@ -45,49 +51,56 @@ public class PlayerController : MonoBehaviour
         JumpAction.Disable();
     }
 
-    void OnCollisionExit(Collision collision)
+    void AdjustDepthPosition(Collider collider)
     {
-        isGrounded = false;
-    }
-
-    float GetBottomY(Collider collider)
-    {
-        return collider.bounds.center.y - collider.bounds.extents.y;
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        float thisBottomY = GetBottomY(m_Collider);
-        foreach (ContactPoint contact in collision.contacts)
+        if (m_Rigidbody.linearVelocity.y > 0f)
         {
-            if (contact.point.y <= thisBottomY + 0.01f)
-            {
-                isGrounded = true;
-                AdjustDepthPosition(collision);
-                break;
-            }
+            return;
         }
-    }
 
-    void AdjustDepthPosition(Collision collision)
-    {
         Vector3 position = transform.position;
-        Bounds colBounds = collision.collider.bounds;
+        Bounds colBounds = collider.bounds;
 
         if (rotated)
         {
             position.x = Mathf.Clamp(position.x, colBounds.min.x, colBounds.max.x);
+            position.y -= GetDistanceDownByTilt(transform.position.x, position.x);
         }
         else
         {
             position.z = Mathf.Clamp(position.z, colBounds.min.z, colBounds.max.z);
+            position.y -= GetDistanceDownByTilt(transform.position.z, position.z);
         }
 
         transform.position = position;
+        PreventFallingThrough(colBounds);
+    }
+
+    float GetDistanceDownByTilt(float oldPlayerAxisPosition, float newPlayerAxisPosition)
+    {
+        float distanceMoved = oldPlayerAxisPosition - newPlayerAxisPosition;
+        float tilt = Mathf.Tan(this.Camera.transform.eulerAngles.x * Mathf.Deg2Rad);
+        return distanceMoved * tilt;
+    }
+
+    void PreventFallingThrough(Bounds colBounds)
+    {
+        if (m_Collider.bounds.min.y < colBounds.max.y && m_Rigidbody.linearVelocity.y < 0f)
+        {
+            Vector3 newVolecity = m_Rigidbody.linearVelocity;
+            newVolecity.y = 0f;
+            m_Rigidbody.linearVelocity = newVolecity;
+
+            Vector3 position = transform.position;
+            position.y = colBounds.max.y + m_Collider.bounds.extents.y;
+            transform.position = position;
+        }
     }
 
     void FixedUpdate()
     {
+        CheckCollision();
+
         float movement = MoveAction.ReadValue<float>();
         Move(movement);
 
@@ -96,6 +109,29 @@ public class PlayerController : MonoBehaviour
 
         bool jump = JumpAction.ReadValue<float>() > 0.5f;
         Jump(jump);
+    }
+
+    void CheckCollision()
+    {
+        Vector3 rayOrigin = Camera.gameObject.transform.position;
+        rayOrigin.y -= m_Collider.bounds.extents.y;
+        Vector3 direction = Camera.gameObject.transform.forward;
+
+        Ray ray = new Ray(rayOrigin, direction);
+        m_Collider.enabled = false;
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            m_Collider.enabled = true;
+            AdjustDepthPosition(hit.collider);
+            isGrounded = Mathf.Approximately(hit.collider.bounds.max.y, m_Collider.bounds.min.y);
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+        m_Collider.enabled = true;
     }
 
     void Move(float movement)
@@ -154,7 +190,6 @@ public class PlayerController : MonoBehaviour
         {
             m_Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             jumpCountdown = JumpCooldown;
-            isGrounded = false;
         }
     }
 
