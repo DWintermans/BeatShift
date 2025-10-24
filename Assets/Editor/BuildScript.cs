96ca8a378df527faffa57c35ba490dd07f48467d
DWintermans <152392649+DWintermans@users.noreply.github.com>
2025-10-24 09:18:40 +0200

    [HOTFIX] Buildscript to include all scenes in scene list

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,10 +1,15 @@
+using System.Linq;
 using UnityEditor;
 
 public static class BuildScript
 {
     public static void PerformBuild()
     {
-        string[] scenes = { "Assets/Scenes/Prototype.unity" };
+        string[] scenes = EditorBuildSettings.scenes
+            .Where(s => s.enabled)
+            .Select(s => s.path)
+            .ToArray();
         BuildPipeline.BuildPlayer(scenes, "build/WebGL", BuildTarget.WebGL, BuildOptions.None);
     }
 }
+    
