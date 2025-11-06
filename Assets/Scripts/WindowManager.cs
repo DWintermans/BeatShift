f552d577d5abbb3367f888b2687c9caabf90247c
WesleyBurgt <wesleyburgt@gmail.com>
2025-11-06 12:22:28 +0100

    Squashed commit of the following:
    commit f7dbc52512113f151020413406405aeeef7fc878
    Author: WesleyBurgt <wesleyburgt@gmail.com>
    Date:   Thu Nov 6 12:18:31 2025 +0100
    
        [FIX] vertices count
    
        by making background buildings PNGs

--- Assets/Scripts/WindowManager.cs
+++ Assets/Scripts/WindowManager.cs
@@ -5,43 +5,23 @@ public class WindowDestroyer : MonoBehaviour
 {
     [Range(0f, 100f)]
     public float randomDeletePercentage = 50f;
-    public Transform WindowSideZ;
-    public Transform WinowSideNegativeZ;
+    public Transform WindowSide;
 
     void Awake()
     {
-        if (!DestroyWindowsOnBackside(WindowSideZ, Vector3.right, Vector3.forward))
-            RandomlyDeleteWindows(WindowSideZ, randomDeletePercentage);
-        if (!DestroyWindowsOnBackside(WinowSideNegativeZ, Vector3.left, Vector3.back))
-            RandomlyDeleteWindows(WinowSideNegativeZ, randomDeletePercentage);        
+        RandomlyDeleteWindows();
     }
 
-    private bool DestroyWindowsOnBackside(Transform windowsGroup, Vector3 xAxisDirection, Vector3 zAxisDirection)
+    private void RandomlyDeleteWindows()
     {
-        if (!IsVisibleFromAxis(xAxisDirection) && !IsVisibleFromAxis(zAxisDirection))
-        {
-            Destroy(windowsGroup.gameObject);
-            return true;
-        }
+        if (randomDeletePercentage <= 0f) return;
 
-        return false;
-    }
-
-    private bool IsVisibleFromAxis(Vector3 axisDirection)
-    {
-        return Vector3.Dot(transform.forward, axisDirection) > 0.5f;
-    }
-
-    private void RandomlyDeleteWindows(Transform windowsGroup, float percentage)
-    {
-        if (percentage <= 0f) return;
-
-        var windows = windowsGroup.GetComponentsInChildren<Transform>()
-                                  .Where(t => t != windowsGroup)
+        var windows = WindowSide.GetComponentsInChildren<Transform>()
+                                  .Where(t => t != WindowSide)
                                   .Select(t => t.gameObject)
                                   .ToList();
 
-        int countToDelete = Mathf.RoundToInt(windows.Count * (percentage / 100f));
+        int countToDelete = Mathf.RoundToInt(windows.Count * (randomDeletePercentage / 100f));
 
         var randomWindows = windows.OrderBy(_ => Random.value).Take(countToDelete);
 
